c21ab1f8eeadb8e04b200be07a1246a4ce73dca4
Wesley Trader <wesley.trader1272@gmail.com>
2026-03-19 00:25:29 -0300

    commit #1

--- bot_commit.bat
+++ bot_commit.bat
@@ -1,12 +1,12 @@
 @echo off
-title BOT COMMIT AUTOMATICO
+title INICIANDO BOT...
 color 0A
 
-:: SOM AO INICIAR (beep)
-echo ^G
+:: SOM REAL AO INICIAR
+powershell -c (New-Object Media.SoundPlayer 'C:\Windows\Media\notify.wav').PlaySync();
 
 echo ================================
-echo      INICIANDO BOT...
+echo        INICIANDO BOT...
 echo ================================
 timeout /t 2 >nul
 
@@ -15,7 +15,7 @@ set contador=1
 :loop
 
 cls
-title BOT ATIVO ^| TOTAL DE COMMITS: %contador%
+title BOT ATIVO ^| TOTAL: %contador%
 
 echo [%time%] Commit #%contador% OK
 
